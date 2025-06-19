2cfc0df61eb14b0b5ec8f664923e983592f05686
Youssef Metry <youssef.kamal.fawzy@gmail.com>
2025-06-19 23:39:42 +0300

    Trying to Fix login/register UI: use direct CSS link in guest layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,8 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        <link rel="stylesheet" href="https://fitness-production-1a41.up.railway.app/build/assets/app-cf2sI21B.css">
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
